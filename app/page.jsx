282c518aac7ea6bdbe813688744c1fcdcca90cb8
Anjan Karmaker <77603380+anjankarmakar10@users.noreply.github.com>
2024-03-06 16:36:41 +0600

    build sponsors section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Sponsors from "@/components/Sponsors";
 
 export default function Home() {
   return (
     <main className="max-w-[1250px] px-4 mx-auto ">
       <Hero />
+      <Sponsors />
     </main>
   );
 }
--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -1,4 +1,12 @@
 const Sponsors = () => {
-  return <section>Sponsors</section>;
+  return (
+    <section className="flex mt-40 items-center justify-evenly flex-wrap gap-10">
+      <img className="w-[200px] h-auto" src="/s1.png" alt="" />
+      <img className="w-[200px] h-auto" src="/s2.png" alt="" />
+      <img className="w-[200px] h-auto" src="/s3.png" alt="" />
+      <img className="w-[100px] h-auto" src="/s4.png" alt="" />
+      <img className="w-[200px] h-auto" src="/s5.png" alt="" />
+    </section>
+  );
 };
 export default Sponsors;
